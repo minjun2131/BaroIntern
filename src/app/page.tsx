4a31202876ae1ec7d7243e3696f87b6af41e5b09
minjun2131 <whitehodol@naver.com>
2025-03-16 21:44:04 +0900

    fix : Suspense 깜박이는 문제 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,13 +25,7 @@ export default async function Home() {
       {/* 할 일 추가 input과 버튼 */}
       <HydrationBoundary state={dehydrate(queryClient)}>
         <TodoForm />
-        <Suspense
-          fallback={
-            <div className="flex min-h-[50vh] items-center justify-center">
-              <div className="h-12 w-12 animate-spin rounded-full border-4 border-purple-200 border-t-purple-600" />
-            </div>
-          }
-        >
+        <Suspense>
           <TodoList />
         </Suspense>
       </HydrationBoundary>
